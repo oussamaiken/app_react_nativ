2002a1073f817195d40475f2cfd6f3a71a84b8e4
oussama iken <oussamaiken03@gmail.com>
2025-03-12 00:38:25 +0100

    Initial commit

---
+++ app/constants/icons.ts
@@ -0,0 +1,19 @@
+import home from "@/app/assets/icons/home.png";
+import search from "@/app/assets/icons/search.png";
+import person from "@/app/assets/icons/person.png";
+import logo from "@/app/assets/icons/logo.png";
+import save from "@/app/assets/icons/save.png";
+import star from "@/app/assets/icons/star.png";
+import play from "@/app/assets/icons/play.png";
+import arrow from "@/app/assets/icons/arrow.png";
+
+export const icons = {
+  home,
+  search,
+  person,
+  logo,
+  save,
+  star,
+  play,
+  arrow,
+};
---
+++ app/constants/images.ts
@@ -0,0 +1,9 @@
+import bg from "@/app/assets/images/bg.png";
+import highlight from "@/app/assets/images/highlight.png";
+import rankingGradient from "@/app/assets/images/rankingGradient.png";
+
+export const images = {
+  bg,
+  highlight,
+  rankingGradient,
+};
